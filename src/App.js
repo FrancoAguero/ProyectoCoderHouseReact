1cda289d14e5824de17182dc1acbafc5df670025
FrancoAguero <franko465aguero@gmail.com>
2021-09-13 23:11:09 -0300

    itemCount

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 //Components
 import NavBar from './components/NavBar'
 import CardContainer from './components/CardContainer'
+import ItemCount from './components/ItemCount'
 
 //Data
 import data from './assets/data/data'
@@ -23,15 +24,18 @@ const App = () => {
       <NavBar handleHomeContent={handleHomeContent}/>
       <div className="home_content">
         <Grid container spacing={5} justifyContent="center" alignItems="center">
-            {
-              products.map((product) => {
-                return (
-                  <Grid item xs={4}>
-                    <CardContainer key={product.id} img={product.img} title={product.name} price={product.price}/>
-                  </Grid>
-                )
-              })
-            }
+          <Grid item xs={12} alignItems={'center'}>
+            <ItemCount initialState={0}/>
+          </Grid>
+          {
+            products.map((product) => {
+              return (
+                <Grid item xs={4}>
+                  <CardContainer key={product.id} img={product.img} title={product.name} price={product.price}/>
+                </Grid>
+              )
+            })
+          }
         </Grid>
       </div>
     </div>
---
+++ src/components/ItemCount.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+//Material UI
+import { Avatar, ButtonGroup, Button } from '@material-ui/core';
+
+const styles = {
+    container: {
+        display: "flex",
+        justifyContent: 'center',
+        flexDirection: "column",
+        alignItems: 'center'
+    },
+    counter: {
+        margin: '20px',
+        height: '100px',
+        width: '100px',
+        fontSize: 40
+    }
+}
+
+const ItemCount = ({ initialState }) => {
+    const [count, setCount] = useState(initialState)
+
+    const handleClick = (type) => {
+        if(type === 'imcrementar'){
+            setCount(previousState => previousState + 1)
+        }
+        if(type === 'decrementar') {
+            setCount(previousState => previousState - 1)
+        } 
+        if(type === 'resetear') {
+            setCount(initialState)
+        }
+    }
+    
+    return (
+        <div style={styles.container}>
+            <Avatar style={styles.counter}>{count}</Avatar>
+            <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+                <Button onClick={() => handleClick('imcrementar')}> +1 </Button>
+                <Button onClick={() => handleClick('resetear')}> reset </Button>
+                <Button onClick={() => handleClick('decrementar')}> -1 </Button>
+            </ButtonGroup>
+        </div>
+    )
+}
+
+export default ItemCount
